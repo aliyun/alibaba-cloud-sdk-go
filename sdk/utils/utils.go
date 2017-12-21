5175a2aedc03bc3cebb773c0962d8330b7b5998e
Morlay <morlay.null@gmail.com>
2017-12-21 13:50:44 +0800

    Value empty check and stringify non-string value for parameters

--- sdk/utils/utils.go
+++ sdk/utils/utils.go
@@ -98,3 +98,52 @@ func InitStructWithDefaultTag(bean interface{}) {
 		}
 	}
 }
+
+type IZero interface {
+	IsZero() bool
+}
+
+func IsEmptyValue(v reflect.Value) bool {
+	if v.CanInterface() {
+		if zeroChecker, ok := v.Interface().(IZero); ok {
+			return zeroChecker.IsZero()
+		}
+	}
+
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
+	return false
+}
+
+func ConvertToString(v interface{}) (string, error) {
+	if stringer, ok := v.(fmt.Stringer); ok {
+		return stringer.String(), nil
+	}
+	rt := reflect.TypeOf(v)
+	switch rt.Kind() {
+	case reflect.String:
+		return v.(string), nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint8:
+		return fmt.Sprintf("%d", v), nil
+	case reflect.Bool:
+		return strconv.FormatBool(v.(bool)), nil
+	case reflect.Float32:
+		return strconv.FormatFloat(float64(v.(float32)), 'f', -1, 32), nil
+	case reflect.Float64:
+		return strconv.FormatFloat(v.(float64), 'f', -1, 64), nil
+	default:
+		return "", fmt.Errorf("unsupport type")
+	}
+}
