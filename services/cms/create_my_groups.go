6562e2bf7bbf61e1a6f7b8dd8521cf757ae3e82d
haowei.yao <haowei.yao@alibaba-inc.com>
2018-04-25 15:02:58 +0800

    由向召元发起的CMS SDK自动发布, 版本号：1.9.6
    Signed-off-by: haowei.yao <haowei.yao@alibaba-inc.com>

--- services/cms/create_my_groups.go
+++ services/cms/create_my_groups.go
@@ -76,11 +76,12 @@ func (client *Client) CreateMyGroupsWithCallback(request *CreateMyGroupsRequest,
 // CreateMyGroupsRequest is the request struct for api CreateMyGroups
 type CreateMyGroupsRequest struct {
 	*requests.RpcRequest
+	Type          string           `position:"Query" name:"Type"`
+	BindUrl       string           `position:"Query" name:"BindUrl"`
 	GroupName     string           `position:"Query" name:"GroupName"`
 	ServiceId     requests.Integer `position:"Query" name:"ServiceId"`
-	BindUrl       string           `position:"Query" name:"BindUrl"`
-	Type          string           `position:"Query" name:"Type"`
 	ContactGroups string           `position:"Query" name:"ContactGroups"`
+	Options       string           `position:"Query" name:"Options"`
 }
 
 // CreateMyGroupsResponse is the response struct for api CreateMyGroups
