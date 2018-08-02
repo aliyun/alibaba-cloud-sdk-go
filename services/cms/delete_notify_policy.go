f89d40533aa10b95f92ecb33bbf63bda457a259c
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-08-02 10:14:12 +0800

    CMS SDK Auto Released By yixiong.jxy,Version：1.23.7
    Signed-off-by: yixiong.jxy <yixiong.jxy@alibaba-inc.com>

--- services/cms/delete_notify_policy.go
+++ services/cms/delete_notify_policy.go
@@ -78,6 +78,7 @@ type DeleteNotifyPolicyRequest struct {
 	*requests.RpcRequest
 	PolicyType string `position:"Query" name:"PolicyType"`
 	AlertName  string `position:"Query" name:"AlertName"`
+	GroupId    string `position:"Query" name:"GroupId"`
 	Id         string `position:"Query" name:"Id"`
 	Dimensions string `position:"Query" name:"Dimensions"`
 }
@@ -85,11 +86,11 @@ type DeleteNotifyPolicyRequest struct {
 // DeleteNotifyPolicyResponse is the response struct for api DeleteNotifyPolicy
 type DeleteNotifyPolicyResponse struct {
 	*responses.BaseResponse
-	Code    string `json:"code" xml:"code"`
-	Message string `json:"message" xml:"message"`
-	Success string `json:"success" xml:"success"`
-	TraceId string `json:"traceId" xml:"traceId"`
-	Result  int    `json:"result" xml:"result"`
+	Code      string `json:"Code" xml:"Code"`
+	Message   string `json:"Message" xml:"Message"`
+	Success   string `json:"Success" xml:"Success"`
+	RequestId string `json:"RequestId" xml:"RequestId"`
+	Result    int    `json:"Result" xml:"Result"`
 }
 
 // CreateDeleteNotifyPolicyRequest creates a request to invoke DeleteNotifyPolicy API
