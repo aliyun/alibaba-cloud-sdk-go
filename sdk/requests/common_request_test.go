c4d6009e6710757b3f20783c286d37d7bc9ad61c
Jackson Tian <shyvo1987@gmail.com>
2018-11-11 13:05:02 +0800

    Add test cases for common_request.go

--- sdk/requests/common_request_test.go
+++ sdk/requests/common_request_test.go
@@ -1,6 +1,7 @@
 package requests
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,5 +24,47 @@ func Test_CommonRequest_TransToAcsRequest(t *testing.T) {
 	r.TransToAcsRequest()
 
 	assert.Equal(t, "RPC", r.GetStyle())
-	assert.Equal(t, "", r.GetQueries())
+
+	r2 := NewCommonRequest()
+	assert.NotNil(t, r2)
+	r2.PathPattern = "/users/[user]"
+	r2.TransToAcsRequest()
+
+	assert.Equal(t, "ROA", r2.GetStyle())
+}
+
+func Test_CommonRequest_String(t *testing.T) {
+	r := NewCommonRequest()
+	assert.NotNil(t, r)
+	r.TransToAcsRequest()
+
+	assert.Equal(t, "RPC", r.GetStyle())
+
+	r2 := NewCommonRequest()
+	assert.NotNil(t, r2)
+	r2.PathPattern = "/users/[user]"
+	r2.TransToAcsRequest()
+
+	assert.Equal(t, "ROA", r2.GetStyle())
+}
+
+func Test_CommonRequest_BuildUrl(t *testing.T) {
+	r := NewCommonRequest()
+	assert.NotNil(t, r)
+	r.SetDomain("host")
+	r.SetScheme("http")
+
+	r.TransToAcsRequest()
+
+	assert.Equal(t, "http://host/?", r.BuildUrl())
+	r.Port = "8080"
+	assert.Equal(t, "http://host:8080/?", r.BuildUrl())
+}
+
+func Test_CommonRequest_GetBodyReader(t *testing.T) {
+	r := NewCommonRequest()
+	r.TransToAcsRequest()
+	reader := r.GetBodyReader()
+	b, _ := ioutil.ReadAll(reader)
+	assert.Equal(t, "", string(b))
 }
--- sdk/requests/types_test.go
+++ sdk/requests/types_test.go
@@ -1,8 +1,9 @@
 package requests
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewInteger(t *testing.T) {
