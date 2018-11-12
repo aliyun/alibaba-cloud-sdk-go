8b052f5254a484711f2d6e2df61ca06bc661457e
Jackson Tian <shyvo1987@gmail.com>
2018-11-12 15:17:43 +0800

    Add test cases for credentials

---
+++ sdk/auth/credentials/sts_role_arn_credential_test.go
@@ -0,0 +1,16 @@
+package credentials
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRoleArnCredential(t *testing.T) {
+	c := NewStsRoleArnCredential("accessKeyId", "accessKeySecret", "roleArn", "roleSessionName", 3600)
+	assert.Equal(t, "accessKeyId", c.AccessKeyId)
+	assert.Equal(t, "accessKeySecret", c.AccessKeySecret)
+	assert.Equal(t, "roleArn", c.RoleArn)
+	assert.Equal(t, "roleSessionName", c.RoleSessionName)
+	assert.Equal(t, 3600, c.RoleSessionExpiration)
+}
