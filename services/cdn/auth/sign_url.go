5833e6eda47591e840604ccaa1af5e7b372edfb8
Shawnpku <chen8132@gmail.com>
2019-01-22 14:26:19 +0800

    update cdn auth

--- services/cdn/auth/sign_url.go
+++ services/cdn/auth/sign_url.go
@@ -9,8 +9,7 @@ import (
 
 // An URLSigner provides URL signing utilities to sign URLs for Aliyun CDN
 // resources.
-//
-// The signer is safe to use concurrently.
+// authentication document: https://help.aliyun.com/document_detail/85117.html
 type URLSigner struct {
 	authType string
 	privKey  string
@@ -25,52 +24,57 @@ func newURLSigner(authType string, privKey string) *URLSigner {
 }
 
 // Sign returns a signed aliyuncdn url based on authentication type
-func (s URLSigner) Sign(baseURL string, path string, expires time.Time) (string, error) {
-	r, err := url.Parse(baseURL)
+func (s URLSigner) Sign(uri string, expires time.Time) (string, error) {
+	r, err := url.Parse(uri)
 	if err != nil {
-		return "", fmt.Errorf("unable to parse baseurl: %s", r)
+		return "", fmt.Errorf("unable to parse url: %s", uri)
 	}
 
 	switch s.authType {
 	case "a":
-		return aTypeSign(r, path, s.privKey, expires), nil
+		return aTypeSign(r, s.privKey, expires), nil
 	case "b":
-		return bTypeSign(r, path, s.privKey, expires), nil
+		return bTypeSign(r, s.privKey, expires), nil
 	case "c":
-		return cTypeSign(r, path, s.privKey, expires), nil
+		return cTypeSign(r, s.privKey, expires), nil
 	default:
 		return "", fmt.Errorf("invalid authentication type")
 	}
 }
 
 // sign by A type authentication method.
-func aTypeSign(r *url.URL, path string, privateKey string, expires time.Time) string {
+// authentication document: https://help.aliyun.com/document_detail/85113.html
+func aTypeSign(r *url.URL, privateKey string, expires time.Time) string {
 	//rand is a random uuid without "-"
 	rand := GenerateUUID().String()
 	// not use, "0" by default
 	uid := "0"
-	secret := fmt.Sprintf("%s-%d-%s-%s-%s", path, expires.Unix(), rand, uid, privateKey)
+	secret := fmt.Sprintf("%s-%d-%s-%s-%s", r.Path, expires.Unix(), rand, uid, privateKey)
 	hashValue := md5.Sum([]byte(secret))
 	authKey := fmt.Sprintf("%d-%s-%s-%x", expires.Unix(), rand, uid, hashValue)
-	u := &url.URL{Scheme: r.Scheme, Host: r.Host, Path: path}
-	signURL := fmt.Sprintf("%s?auth_key=%s", u.String(), authKey)
-	return signURL
+	if r.RawQuery == "" {
+		return fmt.Sprintf("%s?auth_key=%s", r.String(), authKey)
+	} else {
+		return fmt.Sprintf("%s&auth_key=%s", r.String(), authKey)
+	}
 }
 
 // sign by B type authentication method.
-func bTypeSign(r *url.URL, path string, privateKey string, expires time.Time) string {
+// authentication document: https://help.aliyun.com/document_detail/85114.html
+func bTypeSign(r *url.URL, privateKey string, expires time.Time) string {
 	formatExp := expires.Format("200601021504")
-	secret := privateKey + formatExp + path
+	secret := privateKey + formatExp + r.Path
 	hashValue := md5.Sum([]byte(secret))
-	signURL := fmt.Sprintf("%s%s/%x%s", r.String(), formatExp, hashValue, path)
+	signURL := fmt.Sprintf("%s://%s/%s/%x%s?%s", r.Scheme, r.Host, formatExp, hashValue, r.Path, r.RawQuery)
 	return signURL
 }
 
 // sign by C type authentication method.
-func cTypeSign(r *url.URL, path string, privateKey string, expires time.Time) string {
+// authentication document: https://help.aliyun.com/document_detail/85115.html
+func cTypeSign(r *url.URL, privateKey string, expires time.Time) string {
 	hexExp := fmt.Sprintf("%x", expires.Unix())
-	secret := privateKey + path + hexExp
+	secret := privateKey + r.Path + hexExp
 	hashValue := md5.Sum([]byte(secret))
-	signURL := fmt.Sprintf("%s%x/%s%s", r.String(), hashValue, hexExp, path)
+	signURL := fmt.Sprintf("%s://%s/%x/%s%s?%s", r.Scheme, r.Host, hashValue, hexExp, r.Path, r.RawQuery)
 	return signURL
 }
