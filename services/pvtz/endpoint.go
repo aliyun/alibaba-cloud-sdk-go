89e16b63a5d4607a39a9b1ae0e2ff0c011daca33
sdk-team <sdk-team@alibabacloud.com>
2020-02-14 09:42:44 +0800

    Generated 2018-01-01 for pvtz.

--- services/pvtz/endpoint.go
+++ services/pvtz/endpoint.go
@@ -9,19 +9,7 @@ var EndpointType = "central"
 // GetEndpointMap Get Endpoint Data Map
 func GetEndpointMap() map[string]string {
 	if EndpointMap == nil {
-		EndpointMap = map[string]string{
-			"cn-shenzhen":    "pvtz.cn-shenzhen.aliyuncs.com",
-			"cn-beijing":     "pvtz.cn-beijing.aliyuncs.com",
-			"cn-shanghai":    "pvtz.cn-shanghai.aliyuncs.com",
-			"cn-hongkong":    "pvtz.cn-hongkong.aliyuncs.com",
-			"ap-southeast-1": "pvtz.ap-southeast-1.aliyuncs.com",
-			"ap-southeast-3": "pvtz.ap-southeast-3.aliyuncs.com",
-			"eu-central-1":   "pvtz.eu-central-1.aliyuncs.com",
-			"cn-huhehaote":   "pvtz.cn-huhehaote.aliyuncs.com",
-			"ap-southeast-5": "pvtz.ap-southeast-5.aliyuncs.com",
-			"cn-zhangjiakou": "pvtz.cn-zhangjiakou.aliyuncs.com",
-			"cn-chengdu":     "pvtz.cn-chengdu.aliyuncs.com",
-		}
+		EndpointMap = map[string]string{}
 	}
 	return EndpointMap
 }
