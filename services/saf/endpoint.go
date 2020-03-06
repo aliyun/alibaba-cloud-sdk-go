a675ef3083f39f5cd0f2b7c2e20b260c4bb2664c
sdk-team <sdk-team@alibabacloud.com>
2020-03-06 16:37:21 +0800

    Generated 2019-05-21 for saf.

---
+++ services/saf/endpoint.go
@@ -0,0 +1,22 @@
+package saf
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "regional"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-hangzhou": "saf.cn-shanghai.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
