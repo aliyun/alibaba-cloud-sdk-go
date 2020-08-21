036e5116dbe2996e0ef5c74b4da414ec737d7385
sdk-team <sdk-team@alibabacloud.com>
2020-08-21 17:03:22 +0800

    Generated 2018-12-12, 2018-12-07 for UniMkt.

---
+++ services/unimkt/endpoint.go
@@ -0,0 +1,23 @@
+package unimkt
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "regional"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-shanghai": "cloudcode.cn-shanghai.aliyuncs.com",
+			"cn-hangzhou": "cloudcode.cn-hangzhou.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
