b7ce07802cdb9d11edbde445feab42a7977c4b16
sdk-team <sdk-team@alibabacloud.com>
2020-09-01 17:29:23 +0800

    Generated 2019-01-08 for Config.

---
+++ services/config/endpoint.go
@@ -0,0 +1,24 @@
+package config
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "central"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-shanghai":    "config.cn-shanghai.aliyuncs.com",
+			"ap-southeast-1": "config.ap-southeast-1.aliyuncs.com",
+			"cn-hangzhou":    "config.cn-hangzhou.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
