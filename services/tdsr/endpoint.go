791019308c2c3db53a33e13308e211dcfef63590
sdk-team <sdk-team@alibabacloud.com>
2020-09-08 08:39:26 +0000

    Generated 2020-01-01 for tdsr.

---
+++ services/tdsr/endpoint.go
@@ -0,0 +1,22 @@
+package tdsr
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "regional"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-hangzhou": "lyj.cn-hangzhou.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
