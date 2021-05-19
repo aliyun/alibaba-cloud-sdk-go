685e6b5860a4e996778a1c52dde7a59550017295
sdk-team <sdk-team@alibabacloud.com>
2021-05-19 02:20:40 +0000

    Generated 2020-07-02 for scsp.

---
+++ services/scsp/endpoint.go
@@ -0,0 +1,22 @@
+package scsp
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "central"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-shanghai": "scsp-vpc.cn-shanghai.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
