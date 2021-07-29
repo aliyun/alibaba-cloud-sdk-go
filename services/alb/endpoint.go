c4e27a84bb5f9786fd5969100158908aab5710f8
sdk-team <sdk-team@alibabacloud.com>
2021-07-29 11:34:10 +0000

    Generated 2020-06-16 for Alb.

--- services/alb/endpoint.go
+++ services/alb/endpoint.go
@@ -4,28 +4,12 @@ package alb
 var EndpointMap map[string]string
 
 // EndpointType regional or central
-var EndpointType = "central"
+var EndpointType = "regional"
 
 // GetEndpointMap Get Endpoint Data Map
 func GetEndpointMap() map[string]string {
 	if EndpointMap == nil {
-		EndpointMap = map[string]string{
-			"cn-shenzhen":    "alb.cn-shenzhen.aliyuncs.com",
-			"cn-beijing":     "alb.cn-beijing.aliyuncs.com",
-			"cn-wulanchabu":  "alb.cn-wulanchabu.aliyuncs.com",
-			"ap-south-1":     "alb.ap-south-1.aliyuncs.com",
-			"ap-northeast-1": "alb.ap-northeast-1.aliyuncs.com",
-			"cn-chengdu":     "alb.cn-chengdu.aliyuncs.com",
-			"cn-shanghai":    "alb.cn-shanghai.aliyuncs.com",
-			"cn-hongkong":    "alb.cn-hongkong.aliyuncs.com",
-			"ap-southeast-1": "alb.ap-southeast-1.aliyuncs.com",
-			"ap-southeast-2": "alb.ap-southeast-2.aliyuncs.com",
-			"eu-central-1":   "alb.eu-central-1.aliyuncs.com",
-			"ap-southeast-5": "alb.ap-southeast-5.aliyuncs.com",
-			"us-east-1":      "alb.us-east-1.aliyuncs.com",
-			"cn-zhangjiakou": "alb.cn-zhangjiakou.aliyuncs.com",
-			"cn-hangzhou":    "alb.cn-hangzhou.aliyuncs.com",
-		}
+		EndpointMap = map[string]string{}
 	}
 	return EndpointMap
 }
