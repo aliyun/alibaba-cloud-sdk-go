2cb21a585b925807e9e9d263d14e2a8db8c265c9
sdk-team <sdk-team@alibabacloud.com>
2021-12-30 07:05:00 +0000

    Generated 2021-05-20 for idaas-doraemon.

---
+++ services/idaas-doraemon/endpoint.go
@@ -0,0 +1,22 @@
+package idaas_doraemon
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "central"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"cn-hangzhou": "idaas-doraemon.cn-hangzhou.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
