10b72321aa7fc73e15efbd6b675e25241b9dde13
yndu13 <yndu13@gmail.com>
2022-11-06 15:23:01 +0800

    feat: return accessDeniedDetail in error info

--- sdk/errors/server_error_test.go
+++ sdk/errors/server_error_test.go
@@ -36,6 +36,20 @@ func TestServerErrorWithContent(t *testing.T) {
 	assert.Equal(t, "comment", serverError.Comment())
 	assert.Equal(t, "message", serverError.Message())
 	assert.Equal(t, "SDK.ServerError\nErrorCode: InvalidAK\nRecommend: commentrecommend\nRequestId: request id\nMessage: message\nRespHeaders: map[]", serverError.Error())
+
+	e = NewServerError(400, `{"RequestId":"request id","HostId":"host id","Code":"InvalidAK","Recommend":"recommend","Message":"message","AccessDeniedDetail":{}}`, "comment")
+	assert.NotNil(t, e)
+	serverError, ok = e.(*ServerError)
+	assert.True(t, ok)
+	assert.Equal(t, "SDK.ServerError\nErrorCode: InvalidAK\nRecommend: commentrecommend\nRequestId: request id\nMessage: message\nRespHeaders: map[]\nAccessDeniedDetail: map[]", serverError.Error())
+	e = NewServerError(400, `{"RequestId":"request id","HostId":"host id","Code":"InvalidAK","Recommend":"recommend","Message":"message","AccessDeniedDetail":{"AuthAction":"ram:ListUsers","UserId":123}}`, "comment")
+	assert.NotNil(t, e)
+	serverError, ok = e.(*ServerError)
+	assert.True(t, ok)
+	accessDeniedDetail := serverError.AccessDeniedDetail()
+	assert.Equal(t, "ram:ListUsers", accessDeniedDetail["AuthAction"])
+	assert.Equal(t, float64(123), accessDeniedDetail["UserId"])
+	assert.Equal(t, "SDK.ServerError\nErrorCode: InvalidAK\nRecommend: commentrecommend\nRequestId: request id\nMessage: message\nRespHeaders: map[]\nAccessDeniedDetail: map[AuthAction:ram:ListUsers UserId:%!s(float64=123)]", serverError.Error())
 }
 
 func TestWrapServerError(t *testing.T) {
@@ -64,4 +78,21 @@ func TestWrapServerError(t *testing.T) {
 	m = make(map[string]string)
 	WrapServerError(se, m)
 	assert.Equal(t, "", se.Recommend())
+
+	err = NewServerError(400, `{"Code":"Other","AccessDeniedDetail":{}}`, "comment")
+	se, ok = err.(*ServerError)
+	assert.True(t, ok)
+	assert.Equal(t, "Other", se.ErrorCode())
+	m = make(map[string]string)
+	WrapServerError(se, m)
+	assert.Equal(t, "", se.Recommend())
+	assert.Equal(t, "SDK.ServerError\nErrorCode: Other\nRecommend: comment\nRequestId: \nMessage: {\"Code\":\"Other\",\"AccessDeniedDetail\":{}}\nRespHeaders: map[]\nAccessDeniedDetail: map[]", se.Error())
+	err = NewServerError(400, `{"Code":"Other","AccessDeniedDetail":{"AuthAction":"ram:ListUsers","UserId":123}}`, "comment")
+	se, ok = err.(*ServerError)
+	assert.True(t, ok)
+	assert.Equal(t, "Other", se.ErrorCode())
+	m = make(map[string]string)
+	WrapServerError(se, m)
+	assert.Equal(t, "", se.Recommend())
+	assert.Equal(t, "SDK.ServerError\nErrorCode: Other\nRecommend: comment\nRequestId: \nMessage: {\"Code\":\"Other\",\"AccessDeniedDetail\":{\"AuthAction\":\"ram:ListUsers\",\"UserId\":123}}\nRespHeaders: map[]\nAccessDeniedDetail: map[AuthAction:ram:ListUsers UserId:%!s(float64=123)]", se.Error())
 }
