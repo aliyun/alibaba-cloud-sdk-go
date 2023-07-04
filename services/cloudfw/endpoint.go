ae21549792d4b3ec13f51d90ec079dd4d7d43c63
sdk-team <sdk-team@alibabacloud.com>
2023-07-04 03:30:41 +0000

    Generated 2017-12-07 for Cloudfw.

---
+++ services/cloudfw/endpoint.go
@@ -0,0 +1,23 @@
+package cloudfw
+
+// EndpointMap Endpoint Data
+var EndpointMap map[string]string
+
+// EndpointType regional or central
+var EndpointType = "central"
+
+// GetEndpointMap Get Endpoint Data Map
+func GetEndpointMap() map[string]string {
+	if EndpointMap == nil {
+		EndpointMap = map[string]string{
+			"ap-southeast-1": "cloudfw.ap-southeast-1.aliyuncs.com",
+			"cn-hangzhou":    "cloudfw.cn-hangzhou.aliyuncs.com",
+		}
+	}
+	return EndpointMap
+}
+
+// GetEndpointType Get Endpoint Type Value
+func GetEndpointType() string {
+	return EndpointType
+}
