93ee11af28d14f0fe446d1d4a7747e19b744877c
Jackson Tian <shyvo1987@gmail.com>
2024-08-16 14:55:34 +0800

    improve test cases

--- sdk/auth/credentials/provider/env_test.go
+++ sdk/auth/credentials/provider/env_test.go
@@ -1,4 +1,4 @@
-package provider_test
+package provider
 
 import (
 	"os"
@@ -7,23 +7,21 @@ import (
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials/provider"
 )
 
 func TestEnvResolve(t *testing.T) {
-	p := provider.NewEnvProvider()
-	assert.Equal(t, &provider.EnvProvider{}, p)
+	p := NewEnvProvider()
+	assert.Equal(t, &EnvProvider{}, p)
 	c, err := p.Resolve()
 	assert.Nil(t, c)
 	assert.Nil(t, err)
-	os.Setenv(provider.ENVAccessKeyID, "")
-	os.Setenv(provider.ENVAccessKeySecret, "")
+	os.Setenv(ENVAccessKeyID, "")
+	os.Setenv(ENVAccessKeySecret, "")
 	c, err = p.Resolve()
 	assert.Nil(t, c)
 	assert.EqualError(t, err, "Environmental variable (ALIBABACLOUD_ACCESS_KEY_ID or ALIBABACLOUD_ACCESS_KEY_SECRET) is empty")
-	os.Setenv(provider.ENVAccessKeyID, "AccessKeyId")
-	os.Setenv(provider.ENVAccessKeySecret, "AccessKeySecret")
+	os.Setenv(ENVAccessKeyID, "AccessKeyId")
+	os.Setenv(ENVAccessKeySecret, "AccessKeySecret")
 	c, err = p.Resolve()
 	assert.Nil(t, err)
 	assert.Equal(t, &credentials.AccessKeyCredential{AccessKeyId: "AccessKeyId", AccessKeySecret: "AccessKeySecret"}, c)
