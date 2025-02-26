7c4df941ac345ec3311283f054dc47cba6c0b2c0
nanhe <papaya.dyn@alibaba-inc.com>
2025-02-26 18:30:46 +0800

    feat: support imds v2 in old methods

--- sdk/auth/credentials/providers/instance_metadata.go
+++ sdk/auth/credentials/providers/instance_metadata.go
@@ -2,34 +2,91 @@ package providers
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials"
 )
 
+type ecsRAMRoleCredentials struct {
+	SecurityToken   *string `json:"SecurityToken"`
+	Expiration      *string `json:"Expiration"`
+	AccessKeySecret *string `json:"AccessKeySecret"`
+	AccessKeyId     *string `json:"AccessKeyId"`
+	LastUpdated     *string `json:"LastUpdated"`
+	Code            *string `json:"Code"`
+}
+
 var securityCredURL = "http://100.100.100.200/latest/meta-data/ram/security-credentials/"
+var tokenURL = "http://100.100.100.200/latest/api/token"
+var defaultMetadataTokenDuration = 21600 // 6 hours
+
+type do func(req *http.Request) (*http.Response, error)
+
+var hookDo = func(fn do) do {
+	return fn
+}
+
+type newReuqest func(method, url string, body io.Reader) (*http.Request, error)
+
+var hookNewRequest = func(fn newReuqest) newReuqest {
+	return fn
+}
 
 func NewInstanceMetadataProvider() Provider {
 	return &InstanceMetadataProvider{}
 }
 
 type InstanceMetadataProvider struct {
-	RoleName string
+	RoleName      string
+	DisableIMDSv1 bool
 }
 
-func get(url string) (status int, content string, err error) {
-	resp, err := http.Get(url)
-	if err != nil {
+func (p *InstanceMetadataProvider) getMetadataToken() (metadataToken string, err error) {
+	// PUT http://100.100.100.200/latest/api/token
+	httpRequest, _err := hookNewRequest(http.NewRequest)("PUT", tokenURL, strings.NewReader(""))
+	if _err != nil {
+		if p.DisableIMDSv1 {
+			err = fmt.Errorf("get metadata token failed: %s", _err.Error())
+		}
 		return
 	}
-	defer resp.Body.Close()
+	httpRequest.Header.Set("X-aliyun-ecs-metadata-token-ttl-seconds", strconv.Itoa(defaultMetadataTokenDuration))
 
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	return resp.StatusCode, string(bodyBytes), err
+	httpClient := &http.Client{}
+
+	httpResponse, _err := hookDo(httpClient.Do)(httpRequest)
+	if _err != nil {
+		if p.DisableIMDSv1 {
+			err = fmt.Errorf("get metadata token failed: %s", _err.Error())
+		}
+		return
+	}
+
+	defer httpResponse.Body.Close()
+
+	responseBody, _err := ioutil.ReadAll(httpResponse.Body)
+	if _err != nil {
+		if p.DisableIMDSv1 {
+			err = fmt.Errorf("get metadata token failed: %s", _err.Error())
+		}
+		return
+	}
+
+	if httpResponse.StatusCode != http.StatusOK {
+		if p.DisableIMDSv1 {
+			err = fmt.Errorf("received %d getting metadata token: %s", httpResponse.StatusCode, string(responseBody))
+		}
+		return
+	}
+
+	metadataToken = strings.TrimSpace(string(responseBody))
+	return
 }
 
 func (p *InstanceMetadataProvider) GetRoleName() (roleName string, err error) {
@@ -37,19 +94,45 @@ func (p *InstanceMetadataProvider) GetRoleName() (roleName string, err error) {
 	// so attempt to populate it.
 	// If this call is executed in an environment that doesn't support instance metadata,
 	// it will time out after 30 seconds and return an err.
-	status, roleName, err := get(securityCredURL)
+	httpRequest, err := hookNewRequest(http.NewRequest)("GET", securityCredURL, strings.NewReader(""))
+	if err != nil {
+		err = fmt.Errorf("get role name failed: %s", err.Error())
+		return
+	}
 
+	metadataToken, err := p.getMetadataToken()
+	if err != nil {
+		return
+	}
+	if metadataToken != "" {
+		httpRequest.Header.Set("X-aliyun-ecs-metadata-token", metadataToken)
+	}
+
+	httpClient := &http.Client{}
+
+	httpResponse, err := hookDo(httpClient.Do)(httpRequest)
+	if err != nil {
+		err = fmt.Errorf("get role name failed: %s", err.Error())
+		return
+	}
+
+	defer httpResponse.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(httpResponse.Body)
 	if err != nil {
 		return
 	}
 
-	if status != 200 {
-		err = fmt.Errorf("received %d getting role name: %s", status, roleName)
+	roleName = strings.TrimSpace(string(responseBody))
+
+	if httpResponse.StatusCode != http.StatusOK {
+		err = fmt.Errorf("received %d getting role name: %s", httpResponse.StatusCode, roleName)
 		return
 	}
 
 	if roleName == "" {
-		err = errors.New("unable to retrieve role name, it may be unset")
+		err = fmt.Errorf("unable to retrieve role name, it may be unset")
+		return
 	}
 
 	return
@@ -64,43 +147,52 @@ func (p *InstanceMetadataProvider) Retrieve() (auth.Credential, error) {
 		p.RoleName = roleName
 	}
 
-	status, content, err := get(securityCredURL + p.RoleName)
+	httpRequest, err := hookNewRequest(http.NewRequest)("GET", securityCredURL+p.RoleName, strings.NewReader(""))
 	if err != nil {
-		return nil, err
-	}
-	if status != 200 {
-		return nil, fmt.Errorf("received %d getting security credentials for %s", status, p.RoleName)
-	}
-
-	body := make(map[string]interface{})
-
-	if err := json.Unmarshal([]byte(content), &body); err != nil {
+		err = fmt.Errorf("refresh metadata token err: %s", err.Error())
 		return nil, err
 	}
 
-	accessKeyID, err := extractString(body, "AccessKeyId")
+	metadataToken, err := p.getMetadataToken()
 	if err != nil {
 		return nil, err
 	}
-	accessKeySecret, err := extractString(body, "AccessKeySecret")
-	if err != nil {
-		return nil, err
+	if metadataToken != "" {
+		httpRequest.Header.Set("X-aliyun-ecs-metadata-token", metadataToken)
 	}
-	securityToken, err := extractString(body, "SecurityToken")
-	if err != nil {
-		return nil, err
-	}
-	return credentials.NewStsTokenCredential(accessKeyID, accessKeySecret, securityToken), nil
-}
 
-func extractString(m map[string]interface{}, key string) (string, error) {
-	raw, ok := m[key]
-	if !ok {
-		return "", fmt.Errorf("%s not in map", key)
+	httpClient := &http.Client{}
+
+	httpResponse, err := hookDo(httpClient.Do)(httpRequest)
+	if err != nil {
+		err = fmt.Errorf("refresh metadata token err: %s", err.Error())
+		return nil, err
 	}
-	str, ok := raw.(string)
-	if !ok {
-		return "", fmt.Errorf("%s is not a string in map", key)
+
+	defer httpResponse.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		return nil, err
 	}
-	return str, nil
+
+	if httpResponse.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received %d getting security credentials for %s", httpResponse.StatusCode, p.RoleName)
+	}
+
+	var data ecsRAMRoleCredentials
+	err = json.Unmarshal(responseBody, &data)
+	if err != nil {
+		return nil, fmt.Errorf("refresh metadata err, json.Unmarshal fail: %s", err.Error())
+	}
+
+	if data.AccessKeyId == nil || data.AccessKeySecret == nil || data.SecurityToken == nil {
+		return nil, fmt.Errorf("refresh metadata err, fail to get credentials, response: %s", string(responseBody))
+	}
+
+	if *data.Code != "Success" {
+		return nil, fmt.Errorf("refresh metadata err, Code is not Success, response: %s", string(responseBody))
+	}
+
+	return credentials.NewStsTokenCredential(*data.AccessKeyId, *data.AccessKeySecret, *data.SecurityToken), nil
 }
